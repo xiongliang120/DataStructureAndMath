31571adcb05c9b4b6ee27bff52fc9ccc5c5da6ad
xiongliang120 <xiongliang@qq.comm>
2019-11-24 09:17:40 +0800

    添加二分搜索树的前中后序遍历

--- pratice/src/main/java/com/xiongliang/pratice/BinarySearchTree/BST.java
+++ pratice/src/main/java/com/xiongliang/pratice/BinarySearchTree/BST.java
@@ -1,25 +1,27 @@
 package com.xiongliang.pratice.BinarySearchTree;
 
 /**
+ * 二分搜索树
  *
  * @param <E>
  */
 public class BST<E extends Comparable<E>> {
     private int size;
     private Node rootNode;
-    public class Node{
+
+    public class Node {
         private E e;
         private Node left;
         private Node right;
 
-        public Node(E e,Node left,Node right){
+        public Node(E e, Node left, Node right) {
             this.e = e;
             this.left = left;
             this.right = right;
         }
     }
 
-    public BST(){
+    public BST() {
         rootNode = null;
         size = 0;
     }
@@ -27,34 +29,41 @@ public class BST<E extends Comparable<E>> {
 
     /**
      * 返回元素个数
+     *
      * @return
      */
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
     /**
      * 添加元素
+     *
      * @param e
      */
-    public void add(E e){
-        if(rootNode == null){
-            rootNode = new Node(e,null,null);
-        }else{
-            addNode(rootNode,e);
+    public void add(E e) {
+        if (rootNode == null) {
+            rootNode = new Node(e, null, null);
+        } else {
+            addNode(rootNode, e);
         }
         size++;
     }
 
     //递归判断元素添加位置
-    public void addNode(Node node, E e){
-        if(node == null){
-            node = new Node(e,null,null);
-        }
-        if(e.compareTo(node.e) >0){ // 走右子树
-             addNode(node.right,e);
-        }else if(e.compareTo(node.e) <0){ //走左子树
-             addNode(node.left,e);
+    public void addNode(Node node, E e) {
+        if (e.compareTo(node.e) > 0) { // 走右子树
+            if (node.right == null) {
+                node.right = new Node(e, null, null);
+            } else {
+                addNode(node.right, e);
+            }
+        } else if (e.compareTo(node.e) < 0) { //走左子树
+            if (node.left == null) {
+                node.left = new Node(e, null, null);
+            } else {
+                addNode(node.left, e);
+            }
         }
     }
 
@@ -62,37 +71,70 @@ public class BST<E extends Comparable<E>> {
      * 移除元素
      * @param e
      */
-    public void remove(E e){
+    public void remove(E e) {
         size--;
     }
 
     /**
      * 前序遍历
+     *
      * @return
      */
-    public String preOrder() {
-        return "";
+    public void pre() {
+        System.out.println("\n前序遍历");
+        preOrder(rootNode);
+    }
+
+    public void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print("value=" + node.e);
+        preOrder(node.left);
+        preOrder(node.right);
     }
 
     /**
      * 中序遍历
+     *
      * @return
      */
-    public String inOrder(){
-        return "";
+    public void in() {
+        System.out.println("\n中序遍历");
+        inOrder(rootNode);
+    }
+
+    public void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print("value=" + node.e);
+        inOrder(node.right);
     }
 
     /**
      * 后序遍历
+     *
      * @return
      */
-    public String postOrder(){
-        return "";
+    public void post() {
+        System.out.println("\n后序遍历");
+        postOrder(rootNode);
+    }
+
+    public void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print("value="+node.e);
     }
 
 
-
-    public String toString(){
+    public String toString() {
         return "";
     }
 
--- pratice/src/main/java/com/xiongliang/pratice/BinarySearchTree/Main.java
+++ pratice/src/main/java/com/xiongliang/pratice/BinarySearchTree/Main.java
@@ -1,12 +1,33 @@
 package com.xiongliang.pratice.BinarySearchTree;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String args[]){
         BST<Integer> bst = new BST<>();
-        for (int i=0;i< 15;i++){
-            bst.add(i);
-        }
 
-        System.out.println(bst);
+        /**
+         * source=51source=64source=44source=29source=37source=46source=54
+         * source=33source=78source=88source=37source=12source=80source=40source=12
+         */
+        /*
+        for (int i=0;i< 15;i++){
+            int element = new Random().nextInt(100);
+            System.out.print("source="+element);
+            bst.add(element);
+        }*/
+        bst.add(51);
+        bst.add(64); bst.add(44);bst.add(29);bst.add(37);
+        bst.add(46);bst.add(54);bst.add(33);
+        bst.add(78);bst.add(88);bst.add(37);
+        bst.add(12);bst.add(80);bst.add(40);
+        bst.add(12);
+
+        //前序遍历
+        bst.pre();
+        //中序遍历
+        bst.in();
+        //后序遍历
+        bst.post();
     }
 }
