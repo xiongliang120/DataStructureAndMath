32e888337a68cbcc1d3cb6e01eeb86d07b5753bd
xiongliang120 <xiongliang@qq.comm>
2019-12-04 22:37:50 +0800

    添加堆的实现

--- pratice/src/main/java/com/xiongliang/pratice/ArrayHeap/MaxHeap.java
+++ pratice/src/main/java/com/xiongliang/pratice/ArrayHeap/MaxHeap.java
@@ -1,7 +1,39 @@
 package com.xiongliang.pratice.ArrayHeap;
 
+import com.xiongliang.pratice.DynamicArray.Array;
+
 /**
- * 最大堆 -- 基于动态数组实现
- */
-public class MaxHeap {
+ * 最大堆(最小堆) -- 基于动态数组实现完全二叉树，即元素从左到右排列成树的形状，并且堆还满足任意节点总是大于
+ * 等于其左右子树的节点。
+ *
+ *
+ *
+ *
+ * */
+public class MaxHeap<E extends Comparable<E>> {
+    private Array<E> array;
+    private int size;
+
+    public MaxHeap(){
+         array = new Array<>();
+         size = 0;
+    }
+
+    /**
+     * 添加元素
+     * @param e
+     */
+    public void add(E e){
+
+    }
+
+    /**
+     * 删除元素
+     * @param e
+     */
+    public void remove(E e){
+
+    }
+
+
 }
