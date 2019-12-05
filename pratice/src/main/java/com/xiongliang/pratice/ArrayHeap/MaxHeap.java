14f68192ba8ef34d531fc3a620744292f3bea440
xiongliang <xiongliang@jmfen.cn>
2019-12-05 09:23:20 +0800

    添加注释

--- pratice/src/main/java/com/xiongliang/pratice/ArrayHeap/MaxHeap.java
+++ pratice/src/main/java/com/xiongliang/pratice/ArrayHeap/MaxHeap.java
@@ -3,7 +3,7 @@ package com.xiongliang.pratice.ArrayHeap;
 import com.xiongliang.pratice.DynamicArray.Array;
 
 /**
- * 最大堆(最小堆) -- 基于动态数组实现完全二叉树，即元素从左到右排列成树的形状，并且堆还满足任意节点总是大于
+ * 最大堆(最小堆) -- 基于动态数组实现完全二叉树，即元素从左到右排列成树的形状(完全二叉树)，并且堆还满足任意节点总是大于
  * 等于其左右子树的节点。
  *
  *
