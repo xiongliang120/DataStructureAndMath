efdae99374e067ff99a2b7fdab2226ff3e864817
xiongliang <xiongliang@jmfen.cn>
2019-12-11 20:42:29 +0800

    增加排序算法

---
+++ pratice/src/main/java/com/xiongliang/pratice/Sort/BubbleSort.java
@@ -0,0 +1,19 @@
+package com.xiongliang.pratice.Sort;
+
+/**
+ * 冒泡排序
+ * 1) 比较相邻元素，如果第一个比第二个大,就交换两个
+ * 2）对每一对相邻元素做1)操作，这样最后的元素会是最大的数
+ * 3) 针对所有元素重复上诉步骤
+ */
+public class BubbleSort {
+    public void sort(int[] sort){
+        if (sort != null && sort.length > 0){
+            for (int i=0;i<sort.length;i++){
+                for (int j=i+1;j<sort.length;j++){
+                    if()
+                }
+            }
+        }
+    }
+}
--- pratice/src/main/java/com/xiongliang/pratice/Sort/Main.java
+++ pratice/src/main/java/com/xiongliang/pratice/Sort/Main.java
@@ -1,5 +1,6 @@
 package com.xiongliang.pratice.Sort;
 
+
 public class Main {
     public static void main(String args[]){
 
