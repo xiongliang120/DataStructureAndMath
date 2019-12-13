849951ef62849a9a7621668c2d8516c3d85f72bb
xiongliang <xiongliang@jmfen.cn>
2019-12-13 09:42:51 +0800

    选择排序实现

--- pratice/src/main/java/com/xiongliang/pratice/Sort/Main.java
+++ pratice/src/main/java/com/xiongliang/pratice/Sort/Main.java
@@ -4,15 +4,15 @@ package com.xiongliang.pratice.Sort;
 public class Main {
     public static void main(String args[]){
         int[] array = new int[]{2,8,4,10,5,23,9,13};
-        BubbleSort bubbleSort = new BubbleSort();
-        bubbleSort.sort(array);
-        bubbleSort.printArray(array);
-//        Integer a = 1;
-//        Integer b =1;
-//        System.out.println("a ="+a.hashCode()+"b="+b.hashCode());
-//
-//        String c = "11";
-//        String d = "11";
-//        System.out.println("c ="+c.hashCode()+"d="+d.hashCode());
+//        System.out.println("冒泡排序");
+//        BubbleSort bubbleSort = new BubbleSort();
+//        bubbleSort.sort(array);
+//        bubbleSort.printArray(array);
+
+        System.out.println("选择排序");
+        SelectionSort selectionSort = new SelectionSort();
+        selectionSort.sort(array);
+        selectionSort.printArray(array);
+
     }
 }
--- pratice/src/main/java/com/xiongliang/pratice/Sort/SelectionSort.java
+++ pratice/src/main/java/com/xiongliang/pratice/Sort/SelectionSort.java
@@ -1,10 +1,33 @@
 package com.xiongliang.pratice.Sort;
 
 /**
+ *  选择排序原理: 首先在未排序序列中找到最大元素, 存放到已排序序列的末尾位置,循环此操作。
  *
  */
 public class SelectionSort {
     public void sort(int[] sort){
+        int maxIndex = 0;
+        int maxSize =0;
+        for (int k =0;k<sort.length;k++){
+            for (int i=maxSize;i<sort.length;i++){ //寻找最大index
+                if(sort[maxIndex] < sort[i]){
+                    maxIndex = i;
+                }
+            }
 
+            //交换最后元素跟最大元素
+            int tmp = sort[maxIndex];
+            sort[maxIndex] = sort[maxSize];
+            sort[maxSize] = tmp;
+            maxSize++;
+        }
+    }
+
+    public void printArray(int[] sort){
+        if(sort != null){
+            for (int i=0;i<sort.length;i++){
+                System.out.print(sort[i]+".");
+            }
+        }
     }
 }
