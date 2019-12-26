fc0260f279d9667b3b93a65addece3bdc38b9b99
xiongliang <xiongliang@jmfen.cn>
2019-12-26 09:12:37 +0800

    归并排序实现

--- pratice/src/main/java/com/xiongliang/pratice/Sort/MergeSort.java
+++ pratice/src/main/java/com/xiongliang/pratice/Sort/MergeSort.java
@@ -5,9 +5,22 @@ package com.xiongliang.pratice.Sort;
  * 递归实现
  *
  * https://blog.csdn.net/qq_36442947/article/details/81612870
+ * https://blog.csdn.net/weixin_39651041/article/details/80010906
  */
 public class MergeSort {
     public int[] sort(int arr[]){
        return null;
+    }
+
+    public void mergeSort(int arr[],int left, int middle,int right){
+
+    }
+
+    public void printArray(int[] sort){
+        if(sort != null){
+            for (int i=0;i<sort.length;i++){
+                System.out.print(sort[i]+".");
+            }
+        }
     }
 }
