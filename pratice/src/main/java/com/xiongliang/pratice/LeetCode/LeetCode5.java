b675a095caf833bdfa3669f37ee4cc10c7f5b7bd
xiongliang <xiongliang@jmfen.cn>
2020-04-30 09:17:16 +0800

    Z 字形变换

---
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode5.java
@@ -0,0 +1,11 @@
+package com.xiongliang.pratice.LeetCode;
+
+/**
+ *  Z 字形变换
+ *  将一个给定字符串根据给定的行数,以从上往下，从左往右进行Z 字形排序, 然后进行从左往右的输出。
+ */
+public class LeetCode5 {
+    public static void main(String args[]){
+
+    }
+}
