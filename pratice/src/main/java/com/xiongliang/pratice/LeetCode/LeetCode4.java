28a792049dbb3c73ad29f7fde25259117100d78a
xiongliang120 <xiongliang@qq.comm>
2020-05-03 21:23:46 +0800

    暴力法 最长不重复字符串

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode4.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode4.java
@@ -1,5 +1,10 @@
 package com.xiongliang.pratice.LeetCode;
 
+import android.util.ArraySet;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 给定一个字符串，请找出其中不含有重复字符的最长子串的长度
  *
@@ -9,6 +14,44 @@ package com.xiongliang.pratice.LeetCode;
  */
 public class LeetCode4 {
     public static void main(String args[]){
+        String str = "1234567890";
+        method1(str);
+    }
 
+    /**
+     *  暴力法
+     */
+    public static void method1(String str){
+        int max =0;
+        int maxStart = 0;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i; j < str.length() ; j++) {
+                 String tmp = str.substring(i,j+1);
+                 if(isRepeat(tmp)){
+                     if(j+1-i > max){
+                         max = j+1-i;
+                         maxStart = i;
+                     }
+                 }
+            }
+        }
+
+        System.out.println("不含重复的最长字符串长度为="+max+"maxStart="+maxStart+"字符串为="+str.substring(maxStart,maxStart+max));
+    }
+
+    /**
+     * 判断字符串是否重复
+     * @param str
+     * @return
+     */
+    public static boolean isRepeat(String str){
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            if(set.contains(str.charAt(i))){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
