07eda17179c828b7ec0f4edd5bb7bf64edef7197
xiongliang120 <xiongliang@qq.comm>
2020-05-03 22:09:41 +0800

    窗口滑动法 查找最长不含重复字符串

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode4.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode4.java
@@ -2,20 +2,27 @@ package com.xiongliang.pratice.LeetCode;
 
 import android.util.ArraySet;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * 给定一个字符串，请找出其中不含有重复字符的最长子串的长度
  *
  * 思路：
- * 1： 暴力法
- * 2： 窗口滑动法
+ * 1： 暴力法， 时间复杂度O(n^3), 空间复杂度O(n)->空间复杂度可以通过从两边往中间遍历判断优化为O(1)
+ *
+ * 2： 窗口滑动法, Map<String,Integer>, char 和 position+1, start和end, 当遇到重复时,更新start值为新重复的值。
+ *   时间复杂度O(n),空间复杂度O(n)
+ *
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/hua-jie-suan-fa-3-wu-zhong-fu-zi-fu-de-zui-chang-z/
  */
 public class LeetCode4 {
     public static void main(String args[]){
         String str = "1234567890";
-        method1(str);
+//        method1(str);
+        method2(str);
     }
 
     /**
@@ -38,6 +45,32 @@ public class LeetCode4 {
 
         System.out.println("不含重复的最长字符串长度为="+max+"maxStart="+maxStart+"字符串为="+str.substring(maxStart,maxStart+max));
     }
+
+
+    /**
+     * 窗口滑动法
+     */
+    public static int method2(String str){
+        int ans = 0; //最长不重复字符串大小
+        int start = 0;
+        Map<String,Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+             if(map.containsKey(str.charAt(i))){
+                 start = i;
+             }
+
+             if((i-start) > ans){
+                 ans = i-start +1;
+             }
+
+             map.put(str.charAt(i)+"",i+1);
+
+        }
+
+        System.out.println("不含重复的最长字符串长度为="+ans+"字符串为="+str.substring(start,ans));
+        return ans;
+    }
+
 
     /**
      * 判断字符串是否重复
