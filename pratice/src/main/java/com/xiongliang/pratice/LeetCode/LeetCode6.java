8513b532530d4515a25a1df396669778d0748ee2
xiongliang120 <xiongliang@qq.comm>
2020-05-04 16:06:24 +0800

    整数反转

---
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode6.java
@@ -0,0 +1,11 @@
+package com.xiongliang.pratice.LeetCode;
+
+/**
+ * 整数反转
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转.
+ */
+public class LeetCode6 {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode7.java
@@ -0,0 +1,8 @@
+package com.xiongliang.pratice.LeetCode;
+
+/**
+ * 字符串转整型
+ *
+ */
+public class LeetCode7 {
+}
