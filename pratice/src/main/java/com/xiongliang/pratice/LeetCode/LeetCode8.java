6520a811becf9f72cddfc2638869507184781970
xiongliang120 <xiongliang@qq.comm>
2020-05-05 22:50:45 +0800

    判断是否是回文数

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode8.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode8.java
@@ -1,11 +1,10 @@
 package com.xiongliang.pratice.LeetCode;
 
 /**
- * 字符串转整型
- *
+ * 判断一个数是否是回文数
  */
-public class LeetCode7 {
+public class LeetCode8 {
     public static void main(String args[]) {
-
+        
     }
 }
