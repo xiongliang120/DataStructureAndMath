1c387c7b6f74ce558e1f471fde7425371ae34156
xiongliang120 <xiongliang@qq.comm>
2020-05-07 23:12:01 +0800

    整数判读是回文数

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode7.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode7.java
@@ -6,10 +6,13 @@ import java.nio.charset.IllegalCharsetNameException;
  * 字符串转整型
  * 对每个字符进行转换
  *
+ * 时间复杂度O(n), n为字符串长度
+ * 空间复杂度O(1)
+ *
  */
 public class LeetCode7 {
     public static void main(String args[]) {
-        String str = "12345678901";
+        String str = "12345678910";
         System.out.println("打印整型="+transformationMethod(str));
     }
 
@@ -22,9 +25,10 @@ public class LeetCode7 {
 
         for (int i = 0; i < str.length() ; i++) {
             int num = str.charAt(i)- '0';
-            if(result*10 * flag + num*flag > Integer.MAX_VALUE){
+
+            if(result > (Integer.MAX_VALUE - num) /10){ //防止result*10 + num 越界
                 result = Integer.MAX_VALUE;
-            }else if(result*10 * flag + num*flag < Integer.MIN_VALUE){
+            }else if(result < (Integer.MIN_VALUE + num)/10){
                 result = Integer.MIN_VALUE;
             }else {
                 result = result * 10 * flag + num * flag;
@@ -32,13 +36,6 @@ public class LeetCode7 {
         }
 
 
-        if(result >Integer.MAX_VALUE){
-            return Integer.MAX_VALUE;
-        }
-
-        if(result < Integer.MIN_VALUE){
-            return Integer.MIN_VALUE;
-        }
         return result;
     }
 }
--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode8.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode8.java
@@ -1,10 +1,38 @@
 package com.xiongliang.pratice.LeetCode;
 
 /**
- * 判断一个数是否是回文数
+ * 判断一个整数是否是回文数
+ * 解法1：
+ * 将整数转字符串,循环数组一半的长度进行判断对应元素是否相等。
+ *
+ * 解法2：
+ * 将数字得首位和尾位进行比较，依次进行
+ *
+ * 时间复杂度O(log10(n)), n 为整数大小
+ * 空间复杂度O(1)
  */
 public class LeetCode8 {
     public static void main(String args[]) {
+         int tmp = 121211;
+         System.out.println("打印是否是回文整数="+compareStartAndEnd(tmp));
+    }
 
+    public static boolean compareStartAndEnd(int num){
+        int div =1; //数字的位数
+        while (num /div >= 10){
+            div = div * 10;
+        }
+
+        while (num >= 10){
+            int start = num / div;
+            int end = num % 10;
+            if(start == end){
+                num = (num - start*div)/10;
+                div = div/100;
+            }else{
+                return false;
+            }
+        }
+        return true;
     }
 }
