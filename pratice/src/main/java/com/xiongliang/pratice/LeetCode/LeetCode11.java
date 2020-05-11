d978fc8a31b51f2011ed0ce4c2a0e4296ff6f833
xiongliang120 <xiongliang@qq.comm>
2020-05-11 22:46:00 +0800

    整数转罗马数字题目描述

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode11.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode11.java
@@ -2,6 +2,12 @@ package com.xiongliang.pratice.LeetCode;
 
 /**
  * 整数转罗马数字
+ * 罗马数字：
+ *  I,V,X,L,C,D,M 分别对应 1,5,10.50,100,500,1000
+ *
+ *  题目描述:
+ *  给定一个整数,将其转为罗马数字, 输入确保在1到3999的范围内。
+ *  
  */
 public class LeetCode11 {
     public static void main(String args[]) {
