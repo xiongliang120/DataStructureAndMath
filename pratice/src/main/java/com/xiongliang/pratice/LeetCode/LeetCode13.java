1a1feceeeed019b61f320a5e5615028f7ee169cf
xiongliang120 <xiongliang@qq.comm>
2020-05-13 22:57:17 +0800

    最长公共前缀

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode13.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode13.java
@@ -2,11 +2,28 @@ package com.xiongliang.pratice.LeetCode;
 
 /**
  * 最长公共前缀
- * 编写一个函数来查找字符串数组中的最长公共前缀, 如果不存在公共前缀,返回空字符串""
+ * 编写一个函数来查找字符串数组中的最长公共前缀, 如果不存在公共前缀,返回空字符串"".
+ * 所有输入只包含小写字母a_z.
  */
 
 public class LeetCode13 {
     public static void main(String args[]) {
 
+    }
+
+
+    /**
+     * 最长公共字符串
+     * @param str
+     * @return
+     */
+    public static String maxStr(String[] str){
+        String max = str[0];
+
+        for (int i = 1; i < str.length ; i++) {
+            
+        }
+
+        return "";
     }
 }
