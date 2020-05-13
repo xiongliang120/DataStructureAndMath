5955e7930e75d12a297416aee972c2ffecae6d6e
xiongliang <xiongliang@jmfen.cn>
2020-05-13 09:19:37 +0800

    最长公共串以及三数之和

---
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode13.java
@@ -0,0 +1,12 @@
+package com.xiongliang.pratice.LeetCode;
+
+/**
+ * 最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀, 如果不存在公共前缀,返回空字符串""
+ */
+
+public class LeetCode13 {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode14.java
@@ -0,0 +1,11 @@
+package com.xiongliang.pratice.LeetCode;
+
+/**
+ * 三数之和
+ * 给你一个包含n个整数的数组nums, 判断nums中是否存在三个元素a,b,c, 使得a+b+c =0, 找出所有满足条件且不重复的三元组
+ */
+public class LeetCode14 {
+    public static void main(String args[]) {
+
+    }
+}
