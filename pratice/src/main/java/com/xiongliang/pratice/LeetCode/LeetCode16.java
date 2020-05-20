4df8c583414ef946d4e9da5f8ef54b27cc8edc53
xiongliang120 <xiongliang@qq.comm>
2020-05-20 22:52:32 +0800

    电话号码字母组合

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode16.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode16.java
@@ -2,6 +2,13 @@ package com.xiongliang.pratice.LeetCode;
 
 /***
  * 电话号码的字母组合
+ *  给定一个仅包含数字2-9的字符串,返回所有它能表示的字母组合。
+ *  1 不对应任何字母。 2对应a/b/c,3对应d/e/f,4对应g/h/i, 等
+ *
+ *  解题思路：
+ *  这是排列组合问题, 可以通过树的形式表示出来。每个数字对应3个分支， 可以转化为从根
+ *  节点到空节点右多少路径?
+ *  
  */
 public class LeetCode16 {
     public static void main(String args[]) {
