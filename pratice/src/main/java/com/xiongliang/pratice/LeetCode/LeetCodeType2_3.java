ad8ddbf5f3b3b3156c64de7e1be2b051c332e476
xiongliang120 <xiongliang@qq.comm>
2020-06-26 16:52:34 +0800

    删除链表倒数第N个元素

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCodeType2_3.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCodeType2_3.java
@@ -1,7 +1,70 @@
 package com.xiongliang.pratice.LeetCode;
 
 /***
- * 合并两个有序链表
+ *  合并两个有序链表
+ *  描述：将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
  */
 public class LeetCodeType2_3 {
+    public static void main(String args[]) {
+        ListNode oneNode = new ListNode(7);
+        ListNode oneNode1 = new ListNode(8);
+        ListNode oneNode2 = new ListNode(9);
+        ListNode oneNode3 = new ListNode(10);
+
+        ListNode twoNode = new ListNode(11);
+        ListNode twoNode1 = new ListNode(12);
+        ListNode twoNode2 = new ListNode(13);
+        ListNode twoNode3 = new ListNode(14);
+
+        oneNode.next = oneNode1;
+        oneNode1.next = oneNode2;
+        oneNode2.next = oneNode3;
+
+
+        twoNode.next = twoNode1;
+        twoNode1.next = twoNode2;
+        twoNode2.next = twoNode3;
+        ListNode resultNode = addNode(oneNode,twoNode);
+
+        //打印结果
+        while (resultNode !=null){
+            System.out.println("node="+resultNode.val);
+            resultNode = resultNode.next;
+        }
+
+    }
+
+    public static ListNode addNode(ListNode oneNode,ListNode twoNode){
+            ListNode preNode = new ListNode(0);
+            ListNode resultNode = preNode;
+            while (oneNode != null || twoNode != null){
+                if(oneNode == null){
+                    resultNode.next = twoNode;
+                    twoNode = twoNode.next;
+                }else if(twoNode == null){
+                    resultNode.next = oneNode;
+                    oneNode = oneNode.next;
+                }else{
+                    if(oneNode.val > twoNode.val){
+                        resultNode.next = twoNode;
+                        twoNode = twoNode.next;
+                    }else if(oneNode.val == twoNode.val){
+                        resultNode.next = oneNode;
+                        oneNode = oneNode.next;
+                        twoNode = twoNode.next;
+                    }else{
+                        resultNode.next = oneNode;
+                        oneNode = oneNode.next;
+                    }
+                }
+                resultNode = resultNode.next;
+            }
+            return preNode;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
 }
--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCodeType2_4.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCodeType2_4.java
@@ -2,6 +2,77 @@ package com.xiongliang.pratice.LeetCode;
 
 /***
  * 旋转链表
+ * 描述：给定一个链表,将链表每个节点向右移动k个位置,其中k 为非负数
+ *
+ * 示例：
+ * 1->2->3->4->5->NULL, k=2
+ * 4->5->1->2->3->NULL.
+ *
+ * 思路：
+ * 将旧链表连接成换,找到新的链表头尾，然后断开环。
+ * 新的尾节点为 (n-k) % n
+ *
+ * 时间复杂度 O(n)
+ * 空间复杂度O(1)
  */
+
 public class LeetCodeType2_4 {
+    public static void main(String args[]) {
+        ListNode oneNode = new ListNode(7);
+        ListNode oneNode1 = new ListNode(8);
+        ListNode oneNode2 = new ListNode(9);
+        ListNode oneNode3 = new ListNode(10);
+        ListNode oneNode4 = new ListNode(11);
+        ListNode oneNode5 = new ListNode(12);
+        ListNode oneNode6 = new ListNode(13);
+
+        oneNode.next = oneNode1;
+        oneNode1.next = oneNode2;
+        oneNode2.next = oneNode3;
+        oneNode3.next = oneNode4;
+        oneNode4.next = oneNode5;
+        oneNode5.next = oneNode6;
+
+        ListNode resultNode = rotateListNode(oneNode,2);
+
+        //打印结果
+        while (resultNode !=null){
+            System.out.println("node="+resultNode.val);
+            resultNode = resultNode.next;
+        }
+
+
+    }
+
+    /**
+     * 旋转链表
+     */
+    public static ListNode rotateListNode(ListNode headNode,int k){
+        if(headNode == null || headNode.next == null){
+            return headNode;
+        }
+
+        ListNode tailNode = headNode;  //首尾相连
+        int n;
+        for (n=1; tailNode.next != null; n++){
+             tailNode = tailNode.next;
+        }
+        tailNode.next = headNode;
+
+        ListNode  newtailNode = headNode;  //寻找新尾节点
+        ListNode  newheadNode ;   //寻找新的头节点
+        for (int i=1; i< (n-k% n -1);i++){
+             newtailNode = newtailNode.next;
+        }
+        newheadNode = newtailNode.next;
+        newtailNode.next = null;
+
+        return newheadNode;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
 }
