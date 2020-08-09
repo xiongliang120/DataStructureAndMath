6c8a58ea1e1600c6878273386fd28b688c25aada
xiongliang120 <xiongliang@qq.comm>
2020-08-09 11:36:16 +0800

    二叉树中序遍历 栈实现

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCodeType3_2.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCodeType3_2.java
@@ -7,53 +7,62 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- *  二叉树的中序遍历
- *
- *  方式：
- *  1: 递归处理, 先左,后根,最后右
- *
- *  2：栈
- *
- *
+ * 二叉树的中序遍历
+ * <p>
+ * 方式：
+ * 1: 递归处理, 先左,后根,最后右
+ * <p>
+ * 2：栈
  */
 public class LeetCodeType3_2 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         BST<Integer> bst = new BST<>();
         bst.add(51);
-        bst.add(64); bst.add(44);bst.add(29);bst.add(37);
-        bst.add(46);bst.add(54);bst.add(33);
-        bst.add(78);bst.add(88);bst.add(37);
-        bst.add(12);bst.add(80);bst.add(40);
+        bst.add(64);
+        bst.add(44);
+        bst.add(29);
+        bst.add(37);
+        bst.add(46);
+        bst.add(54);
+        bst.add(33);
+        bst.add(78);
+        bst.add(88);
+        bst.add(37);
+        bst.add(12);
+        bst.add(80);
+        bst.add(40);
         bst.add(12);
 
         //中序遍历
         bst.in();
 
-//        orderByStack(bst);
+        orderByStack(bst);
     }
 
     /**
      * 基于栈的中序遍历
      */
-    public static void orderByStack(BST<Integer> root){
+    public static void orderByStack(BST<Integer> root) {
         List<Integer> list = new ArrayList<>();
         BST.Node current = root.getRootNode();
         Stack<BST.Node> stack = new Stack<>();
 
-        while (current != null || !stack.isEmpty()){
-            while (current != null){   //先遍历所有左节点
+
+        //利用栈先进后出的特性
+        while (current != null || !stack.isEmpty()) {
+
+            while (current != null) {
                 stack.push(current);
                 current = current.left;
             }
 
-            current = stack.pop(); //获取所有左节点
+            current = stack.pop();
             list.add((Integer) current.e);
-
-            current = current.right;  //
+            current = current.right;
         }
 
-        for (int i=0;i<list.size();i++){
-            System.out.println("中序遍历="+list.get(i));
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("中序遍历=" + list.get(i));
         }
     }
 }
