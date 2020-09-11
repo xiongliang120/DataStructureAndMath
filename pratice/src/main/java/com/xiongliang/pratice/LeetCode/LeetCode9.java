25324c8f9371c1a7d1c79c1522843af4674b3889
xiongliang <xiongliang@jmfen.cn>
2020-09-11 14:33:47 +0800

    判断两个链表是否相交

---
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode9.java
@@ -0,0 +1,34 @@
+package com.xiongliang.pratice.LeetCode;
+
+/**
+ * 判断两个栈是否相交
+ *
+ * 1.暴力法, 遍历A链表 存储到集合中, 然后遍历B 链表,然后判断是否有元素在集合中
+ *
+ * 2.栈, 使用两个栈A,B, 分别存储A链表,B链表,判断栈顶元素是否相同,相同则相交
+ */
+public class LeetCode9 {
+    public static void main(String args[]) {
+
+    }
+
+
+    /**
+     * 暴力法
+     * @return
+     */
+    public boolean method1(){
+        return false;
+    }
+
+    /**
+     * 栈
+     * @return
+     */
+    public boolean method2(){
+        return false
+    }
+
+
+
+}
