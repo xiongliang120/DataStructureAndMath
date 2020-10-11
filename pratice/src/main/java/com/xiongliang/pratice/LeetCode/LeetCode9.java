2730186b29db297187e28988dc74d5c95c3cbbe0
xiongliang120 <xiongliang@qq.comm>
2020-10-11 16:13:29 +0800

    判断两个链表相交

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode9.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCode9.java
@@ -6,6 +6,8 @@ package com.xiongliang.pratice.LeetCode;
  * 1.暴力法, 遍历A链表 存储到集合中, 然后遍历B 链表,然后判断是否有元素在集合中
  *
  * 2.栈, 使用两个栈A,B, 分别存储A链表,B链表,判断栈顶元素是否相同,相同则相交
+ *
+ * 3: 两个链表相交, 二者尾部肯定是同一个节点, 分别遍历两个链表, 比较尾节点是否相同即可
  */
 public class LeetCode9 {
     public static void main(String args[]) {
