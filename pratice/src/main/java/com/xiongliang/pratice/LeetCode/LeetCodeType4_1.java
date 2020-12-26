ea83d7a90debe77e9f8c6a0a73574a0c89c4d107
xiongliang <xiongliang@jmfen.cn>
2020-12-26 16:52:26 +0800

    判断两个链表是否相交,添加注释

--- pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCodeType4_1.java
+++ pratice/src/main/java/com/xiongliang/pratice/LeetCode/LeetCodeType4_1.java
@@ -22,7 +22,8 @@ public class LeetCodeType4_1 {
         int[] array = {3, 2, 3, 1, 2, 4, 5, 5, 6};
         int k = 3;
 
-        //默认小顶堆, 通过遍历插入将前面最小的元素都移除, 每次移除元素都会重新进行排序, 保证数顶元素最小,最后只剩下3个元素了,栈顶是最小元素
+        //默认小顶堆, 通过遍历插入将前面最小的元素都移除, 默认容量是K,插入数据大于K 会进行扩容
+        //每次移除元素都会重新进行排序, 保证数顶元素最小,最后只剩下3个元素了,栈顶是最小元素
         PriorityQueue<Integer> heap = new PriorityQueue<>(k);
         for (int i = 0; i < array.length; i++) {
             heap.offer(array[i]);
